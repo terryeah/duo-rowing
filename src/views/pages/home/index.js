bc71ae7ba542e0c1ce4e5dcd899f000e4c63a02b
Ayu0317mi <n0217ayumi@gmail.com>
2024-07-11 20:55:00 +1000

    Test commit Add test element on home

--- src/views/pages/home/index.js
+++ src/views/pages/home/index.js
@@ -5,10 +5,25 @@ export default function Home() {
     const heroSection = document.createElement('section');
 
     heroSection.classList.add('hero');
-    heroSection.innerHTML = `
+
+    /*heroSection.innerHTML = `
         ${hero().outerHTML}
         ${createDivider('left-aligned').outerHTML}
+    `;*/
+
+    const heroComponent = Hero();
+    const dividerComponent = createDivider('left-aligned');
+
+    heroSection.appendChild(heroComponent);
+    heroSection.appendChild(dividerComponent);
+
+    const homeBody = document.createElement('section');
+    homeBody.innerHTML = `
+        <h1>Insert head on home</h1>
     `;
 
-    return heroSection.outerHTML;
+    heroSection.appendChild(homeBody);
+    return heroSection;
+
+    /*return heroSection.outerHTML;*/
 }
