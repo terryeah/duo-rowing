28868fdd3ad72846890fad72fdca04f75211b80b
Ayu0317mi <n0217ayumi@gmail.com>
2024-07-13 16:04:43 +1000

    Test add logo img on footer.js

---
+++ src/views/components/global/footer.js
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return `
+        <div>
+            <img class="footer-logo" src="/images/mobile-menu.svg" alt="Footer logo" width="380" height="286" />
+        </div>
+    `;
+}
