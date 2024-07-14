3bf67a0081ec9a0f14434d11b769a1664e9d71e2
Ayu0317mi <n0217ayumi@gmail.com>
2024-07-15 09:06:57 +1000

    create home-mission.scss file, apply the code on mission.js

--- src/views/pages/home/mission.js
+++ src/views/pages/home/mission.js
@@ -1,9 +1,29 @@
 export default function Mission() {
     const section = document.createElement('section');
 
-    section.className = 'mission';
+    section.className = 'home-mission';
     section.innerHTML = `
         <img src="/images/home_02.webp" alt="Home mission">
+        
+        <div class="image-describer product-description">
+            <p>Mission</p>
+            <div>
+                <p>Lorem ipsum dolor sit amet, <br>
+                consectetur adipiscing elit, sed <br>
+                do eiusmod tempor incididunt.</p>
+                <p>
+                Lorem ipsum dolor sit amet, <br>
+                consectetur adipiscing elit, sed do eiusmod <br>
+                tempor incididunt ut labore et dolore magna aliqua. <br>
+                Ut enim ad minim veniam, quis nostrud exercitation <br>
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
+                 Duis aute irure dolor in reprehenderit in voluptate <br>
+                 velit esse cillum dolore eu fugiat nulla pariatur. <br>
+                 Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+                </p>
+            </div>
+            <p>Find out more</p>
+        </div>
     `;
 
     return section;
