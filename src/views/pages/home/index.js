307034b77bb2e38db9c78d3fb8fe63aa09840011
Ayu0317mi <n0217ayumi@gmail.com>
2024-07-14 12:19:18 +1000

    Test add text and image elements on home, add scss code on duo-rowing.scss

--- src/views/pages/home/index.js
+++ src/views/pages/home/index.js
@@ -12,21 +12,33 @@ export default function Home() {
         ${hero().outerHTML}
         ${createDivider('left-aligned').outerHTML}
 
-        <div class="home-products">
-            <div>
+        <section class="home-products">
+            <div class="image-describer product-description">
                 <p>Products</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-                </p>
-                <button type="button" id="btn">Find out more</button>
+                <div>
+                    <p>Lorem ipsum dolor sit amet, <br>
+                    consectetur adipiscing elit, sed <br>
+                    do eiusmod tempor incididunt.</p>
+                    <p>
+                    Lorem ipsum dolor sit amet, <br>
+                    consectetur adipiscing elit, sed do eiusmod <br>
+                    tempor incididunt ut labore et dolore magna aliqua. <br>
+                    Ut enim ad minim veniam, quis nostrud exercitation <br>
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
+                     Duis aute irure dolor in reprehenderit in voluptate <br>
+                     velit esse cillum dolore eu fugiat nulla pariatur. <br>
+                     Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+                    </p>
+                </div>
+                <p>Find out more</p>
             </div>
             <div>
-                <img src="/images/home_01.webp" alt="Home products">
+                <img src="/images/home_01.webp" alt="Home products" width="485" height="458" />
             </div>
-        </div>
-        <div>Home contents</div>
+        </section>
+        <section>
+            <img src="/images/home_02.webp" alt="Home mission">
+        </section>
     `;
 
     return `
