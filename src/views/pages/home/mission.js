f1755f5ede54b92fb5fc7fca3c820c48852f365c
Ayu0317mi <n0217ayumi@gmail.com>
2024-07-15 09:38:34 +1000

    wrap image and mission description elements by mission-wrapper, position mission-box on image by adding scss code

--- src/views/pages/home/mission.js
+++ src/views/pages/home/mission.js
@@ -3,27 +3,30 @@ export default function Mission() {
 
     section.className = 'home-mission';
     section.innerHTML = `
-        <img src="/images/home_02.webp" alt="Home mission">
-        
-        <div class="image-describer product-description">
-            <p>Mission</p>
-            <div>
-                <p>Lorem ipsum dolor sit amet, <br>
-                consectetur adipiscing elit, sed <br>
-                do eiusmod tempor incididunt.</p>
-                <p>
-                Lorem ipsum dolor sit amet, <br>
-                consectetur adipiscing elit, sed do eiusmod <br>
-                tempor incididunt ut labore et dolore magna aliqua. <br>
-                Ut enim ad minim veniam, quis nostrud exercitation <br>
-                ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
-                 Duis aute irure dolor in reprehenderit in voluptate <br>
-                 velit esse cillum dolore eu fugiat nulla pariatur. <br>
-                 Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-                </p>
+        <div class="mission-container">
+            <img src="/images/home_02.webp" alt="Home mission">
+            <div class="mission-box">
+                <div class="image-describer product-description">
+                    <p>Mission</p>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet, <br>
+                        consectetur adipiscing elit, sed <br>
+                        do eiusmod tempor incididunt.</p>
+                        <p>
+                        Lorem ipsum dolor sit amet, <br>
+                        consectetur adipiscing elit, sed do eiusmod <br>
+                        tempor incididunt ut labore et dolore magna aliqua. <br>
+                        Ut enim ad minim veniam, quis nostrud exercitation <br>
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
+                        Duis aute irure dolor in reprehenderit in voluptate <br>
+                        velit esse cillum dolore eu fugiat nulla pariatur. <br>
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+                        </p>
+                    </div>
+                    <p>Find out more</p>
+                </div>
             </div>
-            <p>Find out more</p>
-        </div>
+        </div>    
     `;
 
     return section;
