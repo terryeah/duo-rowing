90ff8efc73eaf5f7c99a79ed6102e453a67af1ff
Ayu0317mi <n0217ayumi@gmail.com>
2024-07-15 14:49:16 +1000

    add duo-logo image to images assests, add elements on footer.js, display footer elements by adding css on footer.scss

--- src/views/components/global/footer.js
+++ src/views/components/global/footer.js
@@ -1,7 +1,38 @@
 export default function Footer(){
     const footer = document.createElement('footer');
     footer.innerHTML = `
-        <div>Footer content</div>
+        <div class="footer-container">
+            <div class="footer-box">
+                <div class="footer-logo">
+                    <img src="/images/logo-main.png" alt="Duo logo">
+                </div>
+                <div class="contact">
+                    <div class="contact-text">
+                        Contact
+                    </div>
+                    <div class="contact-info">
+                        <p>PH: 0437 849 062</p>
+                        <p>Email: duo.rowing@gmail.com</p>
+                    </div>
+                </div>
+                <div class="index">
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Products</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+                <div class="form">
+                    <div class="form-txt">
+                        Hava a question? Leave a message
+                    </div>
+                    <div class="form-box">
+                        <textarea name="" id="" cols="30" rows="5" placeholder="Write a message!"></textarea>
+                    </div>
+                </div>
+            </div>   
+        </div>
     `;
 
     return footer;
