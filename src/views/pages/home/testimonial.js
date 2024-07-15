d04e7bbbb52ced73a707c8c9a923e5009cf583fe
Ayu0317mi <n0217ayumi@gmail.com>
2024-07-15 23:55:17 +1000

    change html structure for testimonial.js, modify scss code on home-testimonial.scss,change code on home-products.scss

--- src/views/pages/home/testimonial.js
+++ src/views/pages/home/testimonial.js
@@ -4,77 +4,82 @@ export default function Testimonial() {
     section.className = 'home-testimonial';
     section.innerHTML = `
         <div class="testimonial-container">
-            <div class="testimonial-box">
-                <div class="image-describer product-description">
+
+            <div class="testimonial-contentsbox">
+                <div class="testimonial-box">
                     <p>Testimonial</p>
-                    <div>
-                        <p>Rolf Volert<br>Vikings Rowing Club</p>
-                    </div>
-                    <div>
-                        <p>Lorem ipsum dolor sit amet, <br>
-                        consectetur adipiscing elit, sed <br>
-                        do eiusmod tempor incididunt.</p>
-                        <p>
-                        Lorem ipsum dolor sit amet, <br>
-                        consectetur adipiscing elit, sed do eiusmod <br>
-                        tempor incididunt ut labore et dolore magna aliqua. <br>
-                        Ut enim ad minim veniam, quis nostrud exercitation <br>
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
-                        Duis aute irure dolor in reprehenderit in voluptate <br>
-                        velit esse cillum dolore eu fugiat nulla pariatur. <br>
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-                        </p>
-                    </div>
+                    <div class="testimonial-textbox">
+                        <div class="textbox-line"></div>
+                        <div class="textbox-title">
+                            <p class="title-main">Rolf Volert</p>
+                            <p>Vikings Rowing Club</p>
+                        </div>
+                        <div class="textbox-text">
+                            <p>Lorem ipsum dolor sit amet, <br>
+                            consectetur adipiscing elit, sed <br>
+                            do eiusmod tempor incididunt.</p>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br>
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                            laboris nisi ut aliquip ex ea commodo consequat. 
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse.
+                            </p>
+                        </div>
+                    </div>    
                 </div>
             </div>
 
-            <div class="testimonial-box">
-                <div class="image-describer product-description">
+            <div class="testimonial-contentsbox">
+                <div class="testimonial-box">
                     <p>Testimonial</p>
-                    <div>
-                        <p>Rolf Volert<br>Vikings Rowing Club</p>
-                    </div>
-                    <div>
-                        <p>Lorem ipsum dolor sit amet, <br>
-                        consectetur adipiscing elit, sed <br>
-                        do eiusmod tempor incididunt.</p>
-                        <p>
-                        Lorem ipsum dolor sit amet, <br>
-                        consectetur adipiscing elit, sed do eiusmod <br>
-                        tempor incididunt ut labore et dolore magna aliqua. <br>
-                        Ut enim ad minim veniam, quis nostrud exercitation <br>
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
-                        Duis aute irure dolor in reprehenderit in voluptate <br>
-                        velit esse cillum dolore eu fugiat nulla pariatur. <br>
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-                        </p>
-                    </div>
+                    <div class="testimonial-textbox">
+                        <div class="textbox-line"></div>
+                        <div class="textbox-title">
+                            <p class="title-main">Rolf Volert</p>
+                            <p>Vikings Rowing Club</p>
+                        </div>
+                        <div class="textbox-text">
+                            <p>Lorem ipsum dolor sit amet, <br>
+                            consectetur adipiscing elit, sed <br>
+                            do eiusmod tempor incididunt.</p>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br>
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                            laboris nisi ut aliquip ex ea commodo consequat. 
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse.
+                            </p>
+                        </div>
+                    </div>    
                 </div>
             </div>
-
-            <div class="testimonial-box">
-                <div class="image-describer product-description">
+            
+            <div class="testimonial-contentsbox">
+                <div class="testimonial-box">
                     <p>Testimonial</p>
-                    <div>
-                        <p>Rolf Volert<br>Vikings Rowing Club</p>
-                    </div>
-                    <div>
-                        <p>Lorem ipsum dolor sit amet, <br>
-                        consectetur adipiscing elit, sed <br>
-                        do eiusmod tempor incididunt.</p>
-                        <p>
-                        Lorem ipsum dolor sit amet, <br>
-                        consectetur adipiscing elit, sed do eiusmod <br>
-                        tempor incididunt ut labore et dolore magna aliqua. <br>
-                        Ut enim ad minim veniam, quis nostrud exercitation <br>
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
-                        Duis aute irure dolor in reprehenderit in voluptate <br>
-                        velit esse cillum dolore eu fugiat nulla pariatur. <br>
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-                        </p>
-                    </div>
+                    <div class="testimonial-textbox">
+                        <div class="textbox-line"></div>
+                        <div class="textbox-title">
+                            <p class="title-main">Rolf Volert</p>
+                            <p>Vikings Rowing Club</p>
+                        </div>
+                        <div class="textbox-text">
+                            <p>Lorem ipsum dolor sit amet, <br>
+                            consectetur adipiscing elit, sed <br>
+                            do eiusmod tempor incididunt.</p>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br>
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                            laboris nisi ut aliquip ex ea commodo consequat. 
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse.
+                            </p>
+                        </div>
+                    </div>    
                 </div>
             </div>
+            
 
         </div>  
     `;
