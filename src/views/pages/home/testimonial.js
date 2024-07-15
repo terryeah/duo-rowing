f970f361c8fb8926f05edb0bf47a2bce8e1bb755
Ayu0317mi <n0217ayumi@gmail.com>
2024-07-15 15:40:16 +1000

    remove unnessesary css from home-testimonial.scss, change text on testimonial.js

--- src/views/pages/home/testimonial.js
+++ src/views/pages/home/testimonial.js
@@ -8,26 +8,8 @@ export default function Testimonial() {
                 <div class="image-describer product-description">
                     <p>Testimonial</p>
                     <div>
-                        <p>Lorem ipsum dolor sit amet, <br>
-                        consectetur adipiscing elit, sed <br>
-                        do eiusmod tempor incididunt.</p>
-                        <p>
-                        Lorem ipsum dolor sit amet, <br>
-                        consectetur adipiscing elit, sed do eiusmod <br>
-                        tempor incididunt ut labore et dolore magna aliqua. <br>
-                        Ut enim ad minim veniam, quis nostrud exercitation <br>
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
-                        Duis aute irure dolor in reprehenderit in voluptate <br>
-                        velit esse cillum dolore eu fugiat nulla pariatur. <br>
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-                        </p>
+                        <p>Rolf Volert<br>Vikings Rowing Club</p>
                     </div>
-                </div>
-            </div>
-
-            <div class="testimonial-box">
-                <div class="image-describer product-description">
-                    <p>Testimonial</p>
                     <div>
                         <p>Lorem ipsum dolor sit amet, <br>
                         consectetur adipiscing elit, sed <br>
@@ -49,6 +31,33 @@ export default function Testimonial() {
             <div class="testimonial-box">
                 <div class="image-describer product-description">
                     <p>Testimonial</p>
+                    <div>
+                        <p>Rolf Volert<br>Vikings Rowing Club</p>
+                    </div>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet, <br>
+                        consectetur adipiscing elit, sed <br>
+                        do eiusmod tempor incididunt.</p>
+                        <p>
+                        Lorem ipsum dolor sit amet, <br>
+                        consectetur adipiscing elit, sed do eiusmod <br>
+                        tempor incididunt ut labore et dolore magna aliqua. <br>
+                        Ut enim ad minim veniam, quis nostrud exercitation <br>
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
+                        Duis aute irure dolor in reprehenderit in voluptate <br>
+                        velit esse cillum dolore eu fugiat nulla pariatur. <br>
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="testimonial-box">
+                <div class="image-describer product-description">
+                    <p>Testimonial</p>
+                    <div>
+                        <p>Rolf Volert<br>Vikings Rowing Club</p>
+                    </div>
                     <div>
                         <p>Lorem ipsum dolor sit amet, <br>
                         consectetur adipiscing elit, sed <br>
