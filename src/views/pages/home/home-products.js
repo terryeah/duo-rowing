29a3b2a10bc6c67911c78ba23fa8e221e5d172b0
Ayu0317mi <n0217ayumi@gmail.com>
2024-07-15 22:47:43 +1000

    change html structure for home-products on home-products.js, modify home-products.scc css code

--- src/views/pages/home/home-products.js
+++ src/views/pages/home/home-products.js
@@ -3,24 +3,33 @@ export default function HomeProducts() {
 
     section.className = 'home-products';
     section.innerHTML = `
-        <div class="image-describer product-description">
-            <p>Products</p>
+        <div>
+            <div>Products</div>
             <div>
-                <p>Lorem ipsum dolor sit amet, <br>
-                consectetur adipiscing elit, sed <br>
-                do eiusmod tempor incididunt.</p>
-                <p>
-                Lorem ipsum dolor sit amet, <br>
-                consectetur adipiscing elit, sed do eiusmod <br>
-                tempor incididunt ut labore et dolore magna aliqua. <br>
-                Ut enim ad minim veniam, quis nostrud exercitation <br>
-                ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
-                 Duis aute irure dolor in reprehenderit in voluptate <br>
-                 velit esse cillum dolore eu fugiat nulla pariatur. <br>
-                 Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-                </p>
+                <div>
+                    <div class="product-textbox">
+                        <div class="textbox-line"></div>
+                        <div class="textbox-title">
+                            Lorem ipsum dolor sit amet, <br>
+                            consectetur adipiscing elit, sed <br>
+                            do eiusmod tempor incididunt.
+                        </div>
+
+                        <p class="textbox-text">
+                        Lorem ipsum dolor sit amet, <br>
+                        consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation <br>
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
+                        Duis aute irure dolor in reprehenderit in voluptate <br>
+                        velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+                        Excepteur sint occaecat
+                        </p>
+                    </div>
+                    <div class="product-button">Find out more</div>
+                </div>
             </div>
-            <p>Find out more</p>
         </div>
         <div>
             <img src="/images/home_01.webp" alt="Home products" width="485" height="458" />
