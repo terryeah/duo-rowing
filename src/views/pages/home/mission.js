4b36574624d891e92907cfae0658aad920fda6e2
Ayu0317mi <n0217ayumi@gmail.com>
2024-07-15 23:29:34 +1000

    change html structure onmission.js,modify home-mission.scss

--- src/views/pages/home/mission.js
+++ src/views/pages/home/mission.js
@@ -5,26 +5,34 @@ export default function Mission() {
     section.innerHTML = `
         <div class="mission-container">
             <img src="/images/home_02.webp" alt="Home mission">
+            <div class="mission-contentsbox">
             <div class="mission-box">
-                <div class="image-describer product-description">
-                    <p>Mission</p>
-                    <div>
-                        <p>Lorem ipsum dolor sit amet, <br>
-                        consectetur adipiscing elit, sed <br>
-                        do eiusmod tempor incididunt.</p>
-                        <p>
+                <div>Mission</div>
+                <div>
+                    <div class="mission-textbox">
+                        <div class="textbox-line"></div>
+                        <div class="textbox-title">
+                            Lorem ipsum dolor sit amet, <br>
+                            consectetur adipiscing elit, sed <br>
+                            do eiusmod tempor incididunt.
+                        </div>
+
+                        <p class="textbox-text">
                         Lorem ipsum dolor sit amet, <br>
-                        consectetur adipiscing elit, sed do eiusmod <br>
-                        tempor incididunt ut labore et dolore magna aliqua. <br>
+                        consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua.
                         Ut enim ad minim veniam, quis nostrud exercitation <br>
                         ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
                         Duis aute irure dolor in reprehenderit in voluptate <br>
-                        velit esse cillum dolore eu fugiat nulla pariatur. <br>
+                        velit esse cillum dolore eu fugiat nulla pariatur.
                         Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-                        </p>
+                        Excepteur sint occaecat
+                        </p>               
                     </div>
-                    <p>Find out more</p>
+                    <div class="mission-button">Find out more</div>
                 </div>
+            
+            </div>
             </div>
         </div>    
     `;
