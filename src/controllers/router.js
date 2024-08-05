f20075f0ee428ab71bba68a5ad8a40de6158e08e
Terry Yang <terryeah7@gmail.com>
2024-08-06 05:21:24 +1000

    Reindented the contact form file

--- src/controllers/router.js
+++ src/controllers/router.js
@@ -5,36 +5,36 @@ import Contact from '../views/pages/contact';
 import { initialiseNav } from '../scripts/components/nav';
 
 export function router() {
-    const path = window.location.pathname;
-    const app = document.querySelector('.app');
-    
-    switch(path) {
-        case '/':
-        case '/home':
-            document.title = 'Duo Rowing';
-            app.innerHTML = Home();
-            break;
-        case '/product':
-            document.title = 'Product - Wheels';
-            app.innerHTML = Product();
-            break;
-        case '/about':
-            document.title = 'About Us';
-            app.innerHTML = About();
-            break;
-        case '/contact':
-            document.title = 'Contact Us';
-            app.innerHTML = Contact();
-            break;
-        default:
-            document.title = 'Duo Rowing';
-            app.innerHTML = Home();
-    }
-    
-    initialiseNav();
+  const path = window.location.pathname;
+  const app = document.querySelector('.app');
+  
+  switch(path) {
+    case '/':
+    case '/home':
+      document.title = 'Duo Rowing';
+      app.innerHTML = Home();
+      break;
+    case '/product':
+      document.title = 'Product - Wheels';
+      app.innerHTML = Product();
+      break;
+    case '/about':
+      document.title = 'About Us';
+      app.innerHTML = About();
+      break;
+    case '/contact':
+      document.title = 'Contact Us';
+      app.innerHTML = Contact();
+      break;
+    default:
+      document.title = 'Duo Rowing';
+      app.innerHTML = Home();
+  }
+  
+  initialiseNav();
 }
 
 export function navigateTo(url) {
-    history.pushState(null, null, url);
-    router();
+  history.pushState(null, null, url);
+  router();
 }
