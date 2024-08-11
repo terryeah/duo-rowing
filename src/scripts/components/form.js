9ad09d187e37ab765a10338bfff9fb3589c387a5
Terry Yang <terryeah7@gmail.com>
2024-08-11 14:22:18 +1000

    Updated the form script formatting

--- src/scripts/components/form.js
+++ src/scripts/components/form.js
@@ -1,30 +1,30 @@
 export function initialiseForm() {
-    const contactForm = document.querySelector('form');
-    const inputField = contactForm.querySelector('textarea');
-    const submitButton = contactForm.querySelector('button');
+  const contactForm = document.querySelector('form')
+  const inputField = contactForm.querySelector('textarea')
+  const submitButton = contactForm.querySelector('button')
 
-    async function handleForm() {
-        if (inputField.value.length === 0) {
-            alert('Please type something in the text field.');
-        } else {
-            await submitForm(inputField.value);
-        }
+  async function handleForm() {
+    if (inputField.value.length === 0) {
+      alert('Please type something in the text field.')
+    } else {
+      await submitForm(inputField.value)
     }
+  }
 
-    function submitForm(value) {
-        contactForm.submit();
-    }
+  function submitForm(value) {
+    contactForm.submit()
+  }
 
-    if (contactForm && submitButton) {
-        submitButton.addEventListener('click', (event) => {
-            event.preventDefault();
-            handleForm();
-        });
-    }
+  if (contactForm && submitButton) {
+    submitButton.addEventListener('click', (event) => {
+      event.preventDefault()
+      handleForm()
+    });
+  }
 }
 
 if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', initialiseForm);
+  document.addEventListener('DOMContentLoaded', initialiseForm)
 } else {
-    initialiseForm();
+  initialiseForm()
 }
