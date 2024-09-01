3dd22e8793f83c758ff64d962628f50788ed9b14
Ayu0317mi <n0217ayumi@gmail.com>
2024-09-01 12:21:07 +1000

    style:set minimum size of font-size as 1 rem on home

--- src/views/pages/home/mission.js
+++ src/views/pages/home/mission.js
@@ -12,8 +12,8 @@ export default function Mission() {
                     <div class="mission-textbox">
                         <div class="textbox-line"></div>
                         <div class="textbox-title">
-                            Lorem ipsum dolor sit amet, <br>
-                            consectetur adipiscing elit, sed <br>
+                            Lorem ipsum dolor sit amet, 
+                            consectetur adipiscing elit, sed
                             do eiusmod tempor incididunt.
                         </div>
 
@@ -25,8 +25,6 @@ export default function Mission() {
                         ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
                         Duis aute irure dolor in reprehenderit in voluptate <br>
                         velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-                        Excepteur sint occaecat
                         </p>               
                     </div>
                     <div class="mission-button">Find out more</div>
